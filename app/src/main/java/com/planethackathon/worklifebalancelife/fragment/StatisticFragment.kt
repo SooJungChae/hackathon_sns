29b8d1d23e18482e9fe6cb7beb6f132b45a48d1c
JaeYoon Song <acornjello14@gmail.com>
2018-09-02 03:18:40 +0900

    statistics layout

--- app/src/main/java/com/planethackathon/worklifebalancelife/fragment/StatisticFragment.kt
+++ app/src/main/java/com/planethackathon/worklifebalancelife/fragment/StatisticFragment.kt
@@ -6,6 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.planethackathon.worklifebalancelife.R
+import com.github.mikephil.charting.charts.LineChart
+import com.planethackathon.worklifebalancelife.common.History
+import java.util.Map
+
 
 class StatisticFragment : Fragment() {
     var mContainer: View? = null
@@ -14,6 +18,19 @@ class StatisticFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         if (mContainer == null) {
             mContainer = inflater.inflate(R.layout.fragment_statistic, container, false)
+
+            val chart = mContainer!!.findViewById(R.id.chart) as LineChart
+            var historyList = mapOf("2018-08-27" to 32400,
+                    "2018-08-28" to 43200,
+                    "2018-08-29" to 43200,
+                    "2018-08-30" to 32400);
+
+
+            for (data in historyList) {
+
+                // turn your data into Entry objects
+                //entries.add(new Entry(data.getValueX(), data.getValueY()));
+            }
         }
 
         return mContainer
